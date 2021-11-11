f8f860c914e38e38a60e4bfc22f018404ec3cb41
Muhammad Yusuf Alvin Mahendra <yusufalvin0799@gmail.com>
2021-11-11 22:35:55 +0700

    menambah export form

---
+++ admin/export-filter.php
@@ -0,0 +1,56 @@
+<?php 
+session_start();
+require_once "../koneksi.php";
+if($_SESSION['login'] != true){
+  header('Location: ../');
+  exit();
+}
+?>
+<?php require_once "template/header.php"; ?>
+
+<?php require_once "template/navbar.php"; ?>
+
+<?php require_once "template/sidebar.php"; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Export Filter</h1>
+          </div>
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-5">
+            <div class="card table-responsive">
+              <div class="card-body">
+                <form action="" method="">
+                  <select name="projects" class="form-select form-control mb-3" aria-label="Default select example">
+                    <option selected>Projects</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                  <button name='export' type="submit" class="btn btn-primary">Export</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+<?php require_once "template/footer.php"; ?>
